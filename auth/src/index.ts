c5d5768049a2e1be5af214c9ebf6594d8b90e1f6
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-13 15:50:55 +0200

    🧪 Refactor: Initial error handler middleware structure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import { json } from 'body-parser';
 
 // Route handlers
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signupRouter } from './routes/signup'
-import { signoutRouter } from './routes/signout'
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+
+// Error handler
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -14,6 +17,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.use(errorHandler);
 
 
 app.listen(3000, () => {
