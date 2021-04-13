d400ffa92a0f76fa1a0125c06b854006507e28af
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-13 17:46:38 +0200

    ADD: Adding a new NotFoundError type and extending from the CutomError class

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,6 +9,7 @@ import { signoutRouter } from './routes/signout';
 
 // Error handler
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -17,6 +18,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all('*', () => {
+   throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
