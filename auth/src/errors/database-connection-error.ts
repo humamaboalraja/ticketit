b55bd6daa299332383e7d860af7f0a18d871a15d
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-13 17:32:46 +0200

    Refactor: Error types abstraction into CustomError abstract class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,8 +1,9 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from './custom-error';
+export class DatabaseConnectionError extends CustomError {
    statusCode = 500;
    reason = 'Error connecting to Database';
    constructor() {
-      super();
+      super('Error connecting to Database');
 
       Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
    }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction} from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
    err: Error,
@@ -10,11 +9,7 @@ export const errorHandler = (
 ) => {
    
    // Checking for error types
-   if(err instanceof RequestValidationError) {
-      return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-   }
-
-   if(err instanceof DatabaseConnectionError) {
+   if(err instanceof CustomError) {
       return res.status(err.statusCode).send({ errors: err.serializeErrors() })
    }
 
