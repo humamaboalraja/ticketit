63eeabf910e0798f39154b445307ac948522d7a8
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-13 17:57:46 +0200

    Add: express-async-errors package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 // Route handlers
@@ -19,7 +20,7 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
-app.all('*', () => {
+app.all('*', async (req, res) => {
    throw new NotFoundError();
 });
 
