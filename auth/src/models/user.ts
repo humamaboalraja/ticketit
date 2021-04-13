f13a10c7a5deed91eb22e08b41438fa43287602a
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-13 21:04:52 +0200

    🔒 Auth: creating UserAttrs interface for effective type checking with typescript & to avoid calling new User directly

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,16 @@
 import mongoose from 'mongoose';
 
+
+/*  
+   Interface: Describing the properties that are
+   required to create a new User
+*/
+interface UserAttrs { 
+   email: string,
+   password: string;
+}
+
+
 const userSchema = new mongoose.Schema({
    email: {
       type: String,
@@ -14,10 +25,8 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-new User ({
-   email: 'test@test.com',
-   password: '123456'
-})
+const buildUser = (attrs: UserAttrs) => {
+   return new User(attrs);
+};
 
-
-export { User };
+export { User, buildUser };
