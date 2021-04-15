424fe80d9c9588d12c28e22c7ab4574c92f0e6a8
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-16 00:17:03 +0200

    🪝 Auth: Adding hooks for the testing env setup

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,3 +1,34 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+
+let mongo = any;
+
+// Hook | Running before all tests
+beforeAll(async () => {
+   mongo = new MongoMemoryServer();
+   const mongoUri = await mongo.getUri();
+
+   await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   });
+});
+
+
+
+// Hook | Running before each test
+beforeEach(async () => {
+   const collections = await mongoose.connection.db.collections();
+   for(let collection of collections){
+      await collection.deleteMany({});
+   }
+});
+
+
+// Hook | Running before each test
+afterAll(async () => {
+   await mongo.stop();
+   await mongoose.connection.close();
+});
