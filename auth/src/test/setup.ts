54862831a5fa5c6fbd411cb91ad356d3e69edf1c
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-16 13:45:30 +0200

    🧪  Auth: adding a globally scoped function get_auth_cookie()

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,13 +1,23 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
 
+// defining a global property
+declare global {
+  namespace NodeJS {
+    interface Global {
+      // Our promise is going to resolve itself with a cookie (string[])
+      get_auth_cookie(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
 // Hook | Running before all tests
 beforeAll(async () => {
-   process.env.JWT_KEY = 'asdfasdf';
+   process.env.JWT_KEY = 'placeholder';
  
    mongo = new MongoMemoryServer();
    const mongoUri = await mongo.getUri();
@@ -36,7 +46,18 @@ afterAll(async () => {
    await mongoose.connection.close();
  });
 
+// Globally scoped function
+global.get_auth_cookie = async () => {
+  const email = 'test@ticketit.com';
+  const password = 'password';
 
-
-
-
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201);
+  
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+}
