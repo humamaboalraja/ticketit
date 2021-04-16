2c410a97c7bb58bb954292a66827063a7a6932fe
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-16 13:43:50 +0200

    🧪  Auth: Refactoring current-user endpoint test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user with a stored cookie after a successful authentication', async () => {
-   const signupResponse =  await request(app)
-      .post('/api/users/signup')
-      .send({
-         email: 'test@ticketit.com',
-         password: 'password'
-      })
-      .expect(201);
-   const cookie = signupResponse.get('Set-Cookie');
+   const cookie = await global.get_auth_cookie();
    
    const response = await request(app)
       .get('/api/users/currentuser')
