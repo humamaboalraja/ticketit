f86c914d3639c609412111a120007b55f3b18dd4
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-16 11:50:21 +0200

    🧪 Auth: testing endpoints responses

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,40 @@ it('returns a 201 successful signup', async () => {
       .expect(201);
 });
 
+
+it('returns a 400 with an invalid email', async() => {
+   return request(app)
+      .post('/api/users/signup')
+      .send({
+         email: 'invalid-input',
+         password: 'password'
+      })
+      .expect(400);
+});
+
+
+it('returns a 400 with an invalid password', async() => {
+   return request(app)
+      .post('/api/users/signup')
+      .send({
+         email: 'invalid-input',
+         password: 'p'
+      })
+      .expect(400);
+}); 
+
+
+it('returns a 400 with email and password', async() => {
+   await request(app)
+      .post('/api/users/signup')
+      .send({
+         email: 'humam@ticketit.com',
+      })
+      .expect(400);
+   await request(app)
+      .post('/api/users/signup')
+      .send({
+         password: '123456'
+      })
+      .expect(400);
+}); 
