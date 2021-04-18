c3980a07809d5aaa829e1978b96a57c1d1e052c1
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-18 21:22:55 +0200

    🖼 Client: Adding signin component

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+export default () => {
+
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const { doRequest, errors } = useRequest({
+      url: '/api/users/signin',
+      method: 'post',
+      body: {
+         email, password
+      },
+      onSuccess: () => Router.push('/')
+   });
+
+   const onSubmit = async event => {
+      event.preventDefault();
+
+      await doRequest();
+}
+
+   return (
+   <form onSubmit={onSubmit}>
+      <h1>Sign in</h1>
+      <div className="form-group">
+         <label>E-mail Adress:</label>
+         <input 
+            value={email} 
+            onChange={ e => setEmail(e.target.value)} 
+            type="text" 
+            name="email" 
+            className="form-control" 
+            placeholder="E.g humam@ticketit.com"
+         />
+      </div>
+      <div className="form-group">
+         <label>Password:</label>
+         <input 
+            value={password}
+            onChange={ e => setPassword(e.target.value)}
+            type="password" 
+            name="password" 
+            className="form-control" 
+            placeholder="Your Password"
+         />
+      </div>
+      {errors}
+      <button className="btn btn-dark btn-block">Sign In</button>
+   </form>
+   )
+}
