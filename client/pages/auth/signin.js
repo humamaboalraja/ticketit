ee3e453709f52e0f2916d033b74023ed6e605a4d
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-18 23:02:21 +0200

    Client: Adding Signout component, App style, header conditional links

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -22,32 +22,43 @@ export default () => {
 }
 
    return (
-   <form onSubmit={onSubmit}>
-      <h1>Sign in</h1>
-      <div className="form-group">
-         <label>E-mail Adress:</label>
-         <input 
-            value={email} 
-            onChange={ e => setEmail(e.target.value)} 
-            type="text" 
-            name="email" 
-            className="form-control" 
-            placeholder="E.g humam@ticketit.com"
-         />
+      <div className="centered container">
+         <div className="col-lg-5">
+         <h1 className="text-center">Sign in 🔒</h1>
+         <br/>
+         <div className="card">
+            <div className="card-body">
+            <form onSubmit={onSubmit}>
+            <div className="form-group">
+               <label>E-mail Adress:</label>
+               <input 
+                  value={email} 
+                  onChange={ e => setEmail(e.target.value)} 
+                  type="text" 
+                  name="email" 
+                  className="form-control" 
+                  placeholder="E.g humam@ticketit.com"
+               />
+            </div>
+            <div className="form-group">
+               <label>Password:</label>
+               <input 
+                  value={password}
+                  onChange={ e => setPassword(e.target.value)}
+                  type="password" 
+                  name="password" 
+                  className="form-control" 
+                  placeholder="Your Password"
+               />
+            </div>
+            {errors}
+            <button className="btn btn-dark btn-block">Sign In</button>
+         </form>
+
+         </div>
+         </div>
+         </div>
       </div>
-      <div className="form-group">
-         <label>Password:</label>
-         <input 
-            value={password}
-            onChange={ e => setPassword(e.target.value)}
-            type="password" 
-            name="password" 
-            className="form-control" 
-            placeholder="Your Password"
-         />
-      </div>
-      {errors}
-      <button className="btn btn-dark btn-block">Sign In</button>
-   </form>
+  
    )
 }
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -22,32 +22,41 @@ export default () => {
 }
 
    return (
-   <form onSubmit={onSubmit}>
-      <h1>Sign up</h1>
-      <div className="form-group">
-         <label>E-mail Adress:</label>
-         <input 
-            value={email} 
-            onChange={ e => setEmail(e.target.value)} 
-            type="text" 
-            name="email" 
-            className="form-control" 
-            placeholder="E.g humam@ticketit.com"
-         />
+      <div className="centered container">
+         <div className="col-lg-5">
+         <h1 className="text-center">Sign up ❤️</h1>
+         <br />
+         <div className="card">
+            <div className="card-body">
+            <form onSubmit={onSubmit}>
+               <div className="form-group">
+                  <label>E-mail Adress:</label>
+                  <input 
+                     value={email} 
+                     onChange={ e => setEmail(e.target.value)} 
+                     type="text" 
+                     name="email" 
+                     className="form-control" 
+                     placeholder="E.g humam@ticketit.com"
+                  />
+               </div>
+               <div className="form-group">
+                  <label>Password:</label>
+                  <input 
+                     value={password}
+                     onChange={ e => setPassword(e.target.value)}
+                     type="password" 
+                     name="password" 
+                     className="form-control" 
+                     placeholder="Your Password"
+                  />
+               </div>
+               {errors}
+               <button className="btn btn-dark btn-block">Sign Up</button>
+            </form>
+            </div>
+         </div>
+         </div>
       </div>
-      <div className="form-group">
-         <label>Password:</label>
-         <input 
-            value={password}
-            onChange={ e => setPassword(e.target.value)}
-            type="password" 
-            name="password" 
-            className="form-control" 
-            placeholder="Your Password"
-         />
-      </div>
-      {errors}
-      <button className="btn btn-dark btn-block">Sign Up</button>
-   </form>
    )
 }
