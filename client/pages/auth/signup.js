0dc94d486a2d2f173d9be2ca4faba1d8b515a17f
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-18 16:36:38 +0200

    🖼 Client: Signup component request, error handling setup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,62 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 export default () => {
-   return <form>
+
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [errors, setErrors] = useState([]);
+
+   const onSubmit = async event => {
+      event.preventDefault();
+
+      try {
+         const response = await axios.post('/api/users/signup', {
+            email, password
+         });
+   
+         console.log(response.data)  
+      } catch(err) {
+         setErrors(err.response.data.errors);
+   }
+}
+
+   return (
+   <form onSubmit={onSubmit}>
       <h1>Sign up</h1>
       <div className="form-group">
          <label>E-mail Adress:</label>
-         <input type="text" name="email" className="form-control" placeholder="E.g humam@ticketit.com"/>
+         <input 
+            value={email} 
+            onChange={ e => setEmail(e.target.value)} 
+            type="text" 
+            name="email" 
+            className="form-control" 
+            placeholder="E.g humam@ticketit.com"
+         />
       </div>
       <div className="form-group">
          <label>Password:</label>
-         <input type="password" name="password" className="form-control" placeholder="Your Password"/>
+         <input 
+            value={password}
+            onChange={ e => setPassword(e.target.value)}
+            type="password" 
+            name="password" 
+            className="form-control" 
+            placeholder="Your Password"
+         />
       </div>
-      <button className="btn btn-primary">Sign Up</button>
+      {errors.length > 0 && (
+      <div className="alert alert-danger">
+         <h4>Opps....</h4>
+         <ul>
+            { errors.map(err => (
+               <li key={err.message}>{err.message}</li>
+            ))}
+         </ul>
+      </div>
+      )}
+      <button className="btn btn-dark btn-block">Sign Up</button>
    </form>
+   )
 }
