6d7adfc5e8edb42a2c40294d4f837bb850db6f7a
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-19 02:15:14 +0200

    Common: @ticketit/common npm package for sharing Logic Between Services

---
+++ common/src/index.ts
@@ -0,0 +1,16 @@
+interface People {
+   first: number;
+   second: number;
+   third: number;
+}
+
+
+const people: People = {
+   first: 1,
+   second: 2,
+   third: 3
+};
+
+console.log(people)
+
+export default people;
