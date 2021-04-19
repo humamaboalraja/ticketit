0931dbfcf7d71dcc9a3ca8d76829438c59f96429
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-19 13:23:26 +0200

    🧪 Tickets: Authenticated users test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,7 +9,11 @@ it('Has a route handler listening to /api/tickets for post requests', async () =
 });
 
 it('Can only be accessed if the user is signed in', async () => {
-
+   const response = await request(app)
+      .post('/api/tickets')
+      .send({})
+      .expect(401);
+   
 });
 
 it('Returns an error if an invalid title is provided', async () => {
