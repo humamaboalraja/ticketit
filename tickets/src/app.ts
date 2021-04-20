7aed886b8c682471fb67911fa63ce28f20818129
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-20 14:18:35 +0200

    Tickets: Adding update router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
 import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 // Error handler
 import { errorHandler, NotFoundError, currentUser } from "@ticketit/common";
@@ -31,6 +32,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 
 app.all('*', async (req, res) => {
