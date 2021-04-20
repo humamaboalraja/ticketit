a06f19507097c04f4192594b1dff6584f8000605
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-20 17:53:41 +0200

    Tickets: adding validation to the ticket update enpoint input values

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,14 +5,30 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+router.put('/api/tickets/:id', 
+   requireAuth, 
+   [
+      body('title')
+         .not()
+         .isEmpty()
+         .withMessage('Title is required'),
+      body('price')
+         .isFloat({ gt: 0})
+         .withMessage('Price must be provided and must be greater than 0')
+   ],
+   validateRequest,
+   async (req: Request, res: Response) => {
 
       const ticket = await Ticket.findById(req.params.id);
 
       if(!ticket) {
          throw new NotFoundError();
       }
-      
+
+      if(ticket.id !== req.currentUser!.id){
+         throw new NotAuthorizedError();
+      }
+
       res.send(ticket);
 });
 
