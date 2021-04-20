8135d60528a3a89dbfd93e5eb942c6f2306e7ec3
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-20 14:19:07 +0200

    Tickets: Update endpoint initial implementation, and passing user authorization test

---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, NotFoundError, validateRequest, NotAuthorizedError } from '@ticketit/common';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+
+      const ticket = await Ticket.findById(req.params.id);
+
+      if(!ticket) {
+         throw new NotFoundError();
+      }
+      
+      res.send(ticket);
+});
+
+
+export { router as updateTicketRouter }
