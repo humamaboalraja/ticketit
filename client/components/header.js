b9033e06aa8d24323310a29d3eebbf2a31420ff7
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-23 10:35:44 +0200

    Client: Adding navbar classes

--- client/components/header.js
+++ client/components/header.js
@@ -2,30 +2,30 @@ import Link from 'next/link';
 
 export default ({ currentUser }) => {
   const links = [
-    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
-    !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'Sign Out', href: '/auth/signout' }
+    !currentUser && { label: 'Sign Up', href: '/auth/signup', className: 'nav-link nav-link-primary' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin', className: 'nav-link' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout', className: 'nav-link'  }
   ]
     .filter(linkConfig => linkConfig)
-    .map(({ label, href }) => {
+    .map(({ label, href, className }) => {
       return (
         <li key={href} className="nav-item">
           <Link href={href}>
-            <a className="nav-link">{label}</a>
+            <a className={className}>{label}</a>
           </Link>
         </li>
       );
     });
-
+    
   return (
      <nav className="navbar navbar-light bg-light">
       <div className="container">
          <Link href="/">
-            <a className="navbar-brand">Ticketit</a>
+            <a className="navbar-brand">◻️ Ticketit</a>
          </Link>
 
          <div className="d-flex justify-content-end">
-            <ul className="nav d-flex align-items-center">{links}</ul>
+            <ul className="nav d-flex align-items-center ">{links}</ul>
          </div>
             </div>
          </nav>
