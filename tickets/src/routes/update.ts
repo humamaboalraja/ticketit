402157b793f101794abcbf4b201dcb4f4df15e02
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-24 18:52:56 +0200

    Tickets: Importing BadRequestError error type

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, NotFoundError, validateRequest, NotAuthorizedError } from '@ticketit/common';
+import { requireAuth, NotFoundError, validateRequest, NotAuthorizedError, BadRequestError } from '@ticketit/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { natsWrapper } from '../nats-wrapper';
@@ -24,6 +24,10 @@ router.put(
      if (!ticket) {
        throw new NotFoundError();
      }
+
+     if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket');
+    }
  
      if (ticket.userId !== req.currentUser!.id) {
        throw new NotAuthorizedError();
