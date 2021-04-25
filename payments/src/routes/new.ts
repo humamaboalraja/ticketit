198ddbbe0c103d6d47ad827182f8fb92e15801bc
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-25 20:47:24 +0200

    Payments: Changing stripe currecny into eur

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -37,7 +37,7 @@ router.post(
     }
 
     const charge = await stripe.charges.create({
-      currency: 'usd',
+      currency: 'eur',
       amount: order.price * 100,
       source: token,
     });
