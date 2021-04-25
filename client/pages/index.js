1af88b00992c4f159f59770e43f136c75d9085f8
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-25 20:23:37 +0200

    Client: Styling landing page components

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,31 +3,28 @@ import Link from 'next/link';
 const LandingPage = ({ currentUser, tickets }) => {
   const ticketList = tickets.map((ticket) => {
     return (
-      <tr key={ticket.id}>
-        <td>{ticket.title}</td>
-        <td>{ticket.price}</td>
-        <td>
-          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
-            <a>View</a>
+      <div className="col-md-6 col-sm-12">
+      <div className="card w-100 mb-4" key={ticket.id}>
+        <div class="card-body">
+        <h3 class="card-title">{ticket.title}</h3>
+        <h6 class="card-subtitle mb-2 text-muted">{ticket.price} $</h6>
+        <hr></hr>
+        <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a className="btn btn-dark btn-block">Purchase Ticket</a>
           </Link>
-        </td>
-      </tr>
+      </div>
+      </div>
+    </div>
     );
   });
 
   return (
-    <div>
-      <h1>Tickets</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>{ticketList}</tbody>
-      </table>
+    <div className="mt-5">
+      <h2 className="mb-3">Tickets</h2>
+      <div className="card-deck">
+
+        {ticketList}
+    </div>
     </div>
   );
 };
