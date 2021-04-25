67a1d98d211b2e3c73036c4d3fe999236f772c99
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-04-25 20:21:33 +0200

    Client: Passing props to the doRequest method

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 export default ({ url, method, body, onSuccess  }) => {
    const [errors, setErrors ] = useState(null);
 
-   const doRequest = async () => {
+   const doRequest = async (props = {}) => {
       try {
         setErrors(null);
-        const response = await axios[method](url, body);
+        const response = await axios[method](url, {...body, ...props});
   
         if (onSuccess) {
           onSuccess(response.data);
@@ -18,7 +18,7 @@ export default ({ url, method, body, onSuccess  }) => {
          setErrors(
             <div className="alert alert-danger">
             <h4>Opps....</h4>
-            <ul>
+            <ul className="my-0">
                {err.response.data.errors.map(err => (
                   <li key={err.message}>{err.message}</li>
                ))}
