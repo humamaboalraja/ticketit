36cfbe2240a4530d9e55942ca2fec4abc7f58c58
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-05-01 07:13:40 +0200

    Auth: Adding emojis to the booting message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const start = async () => {
 
-   console.log('Booting 🍑 🎉 ...');
+   console.log('Booting 🍑 🎉 🚀...');
 
    if(!process.env.JWT_KEY) {
       throw new Error('JWT Key must be defined')
