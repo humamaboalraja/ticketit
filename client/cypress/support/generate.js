4afac5e45674ac3a67610df014196f26aa91f9bb
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-05-04 00:16:27 +0200

    Client: Creating buildUser() method to generate fake users

---
+++ client/cypress/support/generate.js
@@ -0,0 +1,9 @@
+import {build, fake} from 'test-data-bot';
+
+const buildUser = build('User').fields({
+   email: fake(f => f.internet.email()),
+   password: fake(f => f.internet.password())
+});
+
+
+export {buildUser}
