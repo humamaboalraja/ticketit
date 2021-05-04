3fe37907b34c531036f7afca1db470edfb89d236
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-05-04 19:20:40 +0200

    Client: Creating Common @cypress methods to refactor reusable code

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,25 +1,66 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { buildUser } from '../support/generate'
+
+
+Cypress.Commands.add('createUser', overrides => {
+
+   // Create a user
+   const user = buildUser(overrides)
+   cy.request({
+      url: 'https://ticketit.dev/api/users/signup',
+      method: 'POST',
+      body: user
+   }).then(response => ({...response.body.user, ...user}))
+})
+
+
+
+Cypress.Commands.add('signIn', user => {
+   return cy.request({
+      url: 'https://ticketit.dev/api/users/signin',
+      method: 'POST',
+      body: user
+   }).then(response => {
+      document.cookie.indexOf('express:sess');
+      return {...response.body.user, ...user}
+   })
+})
+
+
+
+
+Cypress.Commands.add('assertUrl', (url = '') => {
+   cy.url().should('eq', `${Cypress.config().baseUrl}/${url}`)
+})
+
+
+Cypress.Commands.add('assertLoggedInAs', () => {
+   // it should verify we have an express:sess coockie object
+   cy.window()
+   cy.getCookie('express:sess')
+})
+
+
+Cypress.Commands.add('loginAsNewUser', () => {
+   cy.createUser().then(user => {
+      cy.visit('/');
+      // cy.findByText('Sign Out').click()
+      cy.signIn(user);
+   })
+})
+
+
+
+
+
+Cypress.Commands.add('createTicket', ticket => {
+   cy.findByText('Sell Tickets').click()
+   cy.assertUrl('tickets/new')
+   cy.findByPlaceholderText('E.g NFT').type(ticket.ticket_title);
+   cy.findByPlaceholderText('E.g 20$').type(ticket.price);
+   cy.get('.btn').click();
+   cy.findByText(ticket.ticket_title);
+   cy.window()
+})
+
+
+
