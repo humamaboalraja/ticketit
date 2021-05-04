e4c5ec2d8c42c280bcd368cf7bc4155fcb830714
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-05-04 19:22:58 +0200

    Client: Adding user registration e2e test cases

---
+++ client/cypress/e2e/auth/signup.js
@@ -0,0 +1,49 @@
+import { buildUser } from '../../support/generate'
+describe('Registration', () => {
+   it('Should register a new user ', () => {
+      const user = buildUser()
+
+      cy.visit('/');
+      cy.findByText('Sign Up').click();
+      cy.findByPlaceholderText('E.g humam@ticketit.com').type(user.email);
+      cy.findByPlaceholderText('Your Password').type(user.password);
+      cy.get('.btn').click();
+      cy.assertUrl();
+      cy.assertLoggedInAs(user);
+      cy.findByText('Sign Out')
+   })
+
+   it('Should show and error message if there is an error registering ', () => {
+      cy.server().route({
+         url: 'https://ticketit.dev/auth/signup',
+         method: 'POST',
+         status: 400,
+         response: {},
+      })
+      cy.visit('/auth/signup')
+      cy.get('.btn').click()
+      cy.findByText('Opps....')
+
+   })
+
+   it('Should return an error message if an email is invalid ', () => {
+      const user = buildUser()
+      cy.visit('/');
+      cy.findByText('Sign Up').click();
+      cy.findByPlaceholderText('E.g humam@ticketit.com').type('user.email');
+      cy.findByPlaceholderText('Your Password').type(user.password);
+      cy.get('.btn').click();
+      cy.findByText('Email must be valid')
+   })
+
+   it('Should return an error message if a password is invalid ', () => {
+      const user = buildUser()
+
+      cy.visit('/');
+      cy.findByText('Sign Up').click();
+      cy.findByPlaceholderText('E.g humam@ticketit.com').type(user.email);
+      cy.findByPlaceholderText('Your Password').type('1');
+      cy.get('.btn').click();
+      cy.findByText('Your password should be between 4 and 20 characters')
+   })
+})
