0947eeb79a956cc8e2ecb867120c2c034d463d6b
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-05-04 21:08:37 +0200

    Client: Authenticated user sign Out e2e test

--- client/cypress/e2e/ticketit.js
+++ client/cypress/e2e/ticketit.js
@@ -1,8 +1,10 @@
-describe('App', () => {
-   it('can do things', () => {
-      cy.visit('/')
-         .findByText('a', { name: /Sign Up/i })
-         .click()
+describe('Authenticated User', () => {
+   it('Displays Sign Out', () => {
+      cy.loginAsNewUser().then(() => {
+         cy.visit('/');
+         cy.assertUrl('')
+         cy.findByText('Sign Out').click()
+         cy.findByText('Sign Out').should('not.exist')
+      })
    })
 })
-;
